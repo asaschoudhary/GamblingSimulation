ac56e886d8d1628a5bbc8f496ed2b3cdacd46004
Ashish <asaschoudhary616@gmail.com>
2020-02-29 17:56:38 +0530

    MODIFIDE: Added feature to display loss and profit per day

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 echo "Welcome Gambling simulation"
 #We are taking two constant varible
-stake=100
 bet=1
 #Variable
 win=0
-loss=1
-half=$(($stake/2))
-full=$(($half+stake))
-while((half<stake && full>stake))
+loss=0
+for((day=1;day<=20;day++))
 do
-	#Generate the random value
-	checkRandom=$(($RANDOM%2))
-	case $checkRandom in
-		0)
-			stake=$((stake+bet))
-			((win++))
-			;;
-		1)
-			stake=$((stake-bet))
-			((loss++))
-			;;
-	esac
+	stake=100
+	temp=$stake
+	half=$(($stake/2))
+	full=$(($half+stake))
+	while((half<stake && full>stake))
+	do
+		#Generate the random value
+		checkRandom=$((RANDOM%2))
+		case $checkRandom in
+			0)
+				stake=$((stake+bet))
+				((win++))
+				;;
+			1)
+				stake=$((stake-bet))
+				((loss++))
+				;;
+
+		esac
+	done
+	if(($temp<$stake))
+	then
+		stakeProfit=$((stake-temp))
+		echo "$day- profit:$stakeProfit"
+	else
+		stakeLoss=$((stake-temp))
+		echo "$day- loss:$stakeLoss"
+	fi
 done
-echo $stake
