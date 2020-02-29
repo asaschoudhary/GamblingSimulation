f85b3fa1ba494f6dea4757e53b0922d2814bbaa0
Ashish <asaschoudhary616@gmail.com>
2020-02-29 14:01:05 +0530

    MODIFIED:Find loss and win

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,3 +3,23 @@ echo "Welcome Gambling simulation"
 #We are taking two constant varible
 stake=100
 bet=1
+#Variable
+win=0
+loss=1
+#Generate the random value
+checkRandom=$(($RANDOM%2))
+		
+if ((checkRandom == win))
+then
+	echo "you stake is win"
+	stake=$(($stake+$bet))
+else
+	echo "SORRY, You stake is loss"
+	stake=$(($stake-$bet))
+fi
+echo $stake;
+
+
+
+
+
